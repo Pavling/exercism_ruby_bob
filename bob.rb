781d07b7eaf913b647fe563f1127d0bf02e75e92
Michael Pavling <pavling@gmail.com>
2013-10-04 11:07:10 +0100

    passing test_shouting

--- bob.rb
+++ bob.rb
@@ -2,7 +2,18 @@ class Bob
 
 
   def hey(value)
-    'Whatever.'
+    case
+      when is_shouting?(value)
+        'Woah, chill out!'
+      else
+        'Whatever.'
+    end
+
+  end
+
+  private
+  def is_shouting?(value)
+    value.upcase == value
   end
 
 end
