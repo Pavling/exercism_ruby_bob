e0cf66b313274ed6544bf382ad30f4dbc7a8095c
Michael Pavling <pavling@gmail.com>
2013-10-04 11:12:50 +0100

    passing test_asking_a_question

--- bob.rb
+++ bob.rb
@@ -1,10 +1,11 @@
 class Bob
 
-
   def hey(value)
     case
       when is_shouting?(value)
         'Woah, chill out!'
+      when is_a_question?(value)
+        'Sure.'
       else
         'Whatever.'
     end
@@ -14,6 +15,11 @@ class Bob
   private
   def is_shouting?(value)
     value.upcase == value
+  end
+
+  private
+  def is_a_question?(value)
+    value.respond_to?(:match) && value.match(/\?$/)
   end
 
 end
